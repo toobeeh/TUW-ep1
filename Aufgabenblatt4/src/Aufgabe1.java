b5cff88d548ee934c9bb54235594f7101f2baad2
tobeh <tobiasch@gmx.at>
2021-12-01 21:22:48 +0100

    Solve ab4 ag1

--- Aufgabenblatt4/src/Aufgabe1.java
+++ Aufgabenblatt4/src/Aufgabe1.java
@@ -1,6 +1,5 @@
 /*
     Aufgabe 1) Code Analyse - Eindimensionale Arrays
-    // test github commit
 */
 public class Aufgabe1 {
 
@@ -34,7 +33,7 @@ public class Aufgabe1 {
     }
 
     private static void printArray(int[] workArray) {
-        for (int i = 0; i < workArray.length; i--) {
+        for (int i = 0; i < workArray.length; i++) {
             System.out.print(workArray[i] + " ");
         }
         System.out.println();
@@ -58,12 +57,27 @@ public class Aufgabe1 {
     //**************************************************************************
     //**** Notizen und Fragebeantwortungen bitte hier unterhalb durchführen! ***
     //**************************************************************************
+
     //Frage 1:
-    //
+    // Die Zählervariable in der For-Schleife muss inkrementiert werden.
+
     //Frage 2:
-    //
+    // Der Methode wird der Pointer auf das Array übergeben. Das referenzierte Array wird dann befüllt.
+
     //Frage 3:
-    //
+    // Die Methode arbeitet tatsächlich nicht mit einer Kopie, sondern nur einer Kopie des Pointers.
+    // Daher wird eigentlich das ursprüngliche Array gefiltert.
+
     //Frage 4:
-    //
+    // In der Methode fillArrayWithNewContent wird am Ende nicht das originale Array überschrieben, sondern nur der Array-Pointer auf das originale Array
+    // im Frame der Methode überschrieben.
+
+    // Zusatzfragen:
+    // 1 - Der Index muss eine Ganzzahl sein. Das kann einerseits ein int, aber auch ein Datentyp wie char oder byte sein.
+    // 2 - Wenn mit dem Array gearbeitet werden soll, schon. Allerdings muss nicht jede Position initialisiert werden, das geschieht per Defaults.
+    // 3 - Es kann die Methode Arrays.copyOf verwendet werden. Param. 1 ist das Array, Param.  2 die neue Länge
+    // 4 - Es wird wenn nicht anders verlangt die Mehtode System.arraycopy verwendet.
+    // 5 - Ja.
+    // 6 - Nur, wenn geprüft werden soll ob die Arrays die selben sind - NICHT die gleichen! (Pointer vs Inhalt)
+
 }
