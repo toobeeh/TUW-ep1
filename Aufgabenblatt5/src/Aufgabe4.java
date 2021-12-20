da4abe6b6d641de11bc64e9879241aac3b018462
tobeh <tobiasch@gmx.at>
2021-12-20 18:42:43 +0100

    solve ab5 / ag4 - completed 🥳

--- Aufgabenblatt5/src/Aufgabe4.java
+++ Aufgabenblatt5/src/Aufgabe4.java
@@ -13,20 +13,54 @@ public class Aufgabe4 {
     private static Random myRand = new Random(2021L);
 
     private static Color[][] genLandscape(int size) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
-        return null; //Zeile kann geändert oder entfernt werden.
+        Color[][] landscape = new Color[size][size];
+        for(int y = 0; y < size; y++){
+            for(int x = 0; x < size; x++){
+                landscape[y][x] = myRand.nextDouble() < 0.2 ? Color.GRAY : Color.GREEN;
+            }
+        }
+        return landscape;
     }
 
     private static void drawLandscape(CodeDraw myDrawObj, Color[][] landscape) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        int size = landscape.length;
+        double pixelPerField = canvasSize / size;
+        for(int y = 0; y < size; y++){
+            for(int x = 0; x < size; x++){
+                myDrawObj.setColor(landscape[y][x]);
+                myDrawObj.fillSquare(x * pixelPerField, y * pixelPerField, pixelPerField);
+            }
+        }
     }
 
     private static void simWaterFlow(Color[][] landscape, int row, int col) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        // return if out of bounds
+        if(row >= landscape.length || col >= landscape[0].length || row < 0 || col < 0) return;
+        // is this field a rock? split stream
+        if(landscape[row][col] == Color.GRAY){
+            landscape[row][col] = Color.DARK_GRAY;
+            simWaterFlow(landscape, row, col - 1);
+            simWaterFlow(landscape, row, col + 1);
+        }
+        else if(landscape[row][col] == Color.GREEN || landscape[row][col] == Color.BLUE) {
+            // draw current field
+            int direction = myRand.nextDouble() < 0.5 ? 1 : -1;
+            landscape[row][col] = Color.BLUE;
+            simWaterFlow(landscape, row+1, col + direction);
+        }
     }
 
     private static void simSpreadingFire(Color[][] landscape, int row, int col) {
-        // TODO: Implementieren Sie hier Ihre Lösung für die Angabe
+        // exit conditions: out of bounds, field is not grass
+        if(row >= landscape.length || col >= landscape[0].length
+                || row < 0 || col < 0
+                || landscape[row][col] != Color.GREEN) return;
+        //spread fire N-E-S-W
+        landscape[row][col] = Color.RED;
+        if(myRand.nextDouble() >= 0.3) simSpreadingFire(landscape, row-1,col);
+        if(myRand.nextDouble() >= 0.3) simSpreadingFire(landscape, row,col+1);
+        if(myRand.nextDouble() >= 0.3) simSpreadingFire(landscape, row+1,col);
+        if(myRand.nextDouble() >= 0.3) simSpreadingFire(landscape, row,col-1);
     }
 
     public static void main(String[] args) {
@@ -35,13 +69,16 @@ public class Aufgabe4 {
         int size = 100;
         Color[][] landscape = genLandscape(size);
 
-        drawLandscape(myDrawObj, landscape);
+        //drawLandscape(myDrawObj, landscape);
+        //myDrawObj.show();
 
         simWaterFlow(landscape, 0, size / 2);
-        drawLandscape(myDrawObj, landscape);
+        //drawLandscape(myDrawObj, landscape);
+        //myDrawObj.show();
 
         landscape[20][20] = Color.GREEN;
         simSpreadingFire(landscape, 20, 20);
         drawLandscape(myDrawObj, landscape);
+        myDrawObj.show();
     }
 }
